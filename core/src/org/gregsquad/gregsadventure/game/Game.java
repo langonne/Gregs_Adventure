132a3d6e991f526edf428dbbf036edfd75d09980
?FpAdxF&ci4Xpe <zentoxll@gmail.com>
2023-12-03 20:02:12 +0100

    Add playerList, donjonStack, treasureStack, donjonDiscard, and treasureDiscard to Game class

--- core/src/org/gregsquad/gregsadventure/game/Game.java
+++ core/src/org/gregsquad/gregsadventure/game/Game.java
@@ -1,5 +1,18 @@
 package org.gregsquad.gregsadventure.game;
 
+import java.util.ArrayList;
+
+import org.gregsquad.gregsadventure.card.Card;
+import org.gregsquad.gregsadventure.card.Stack;
+import org.gregsquad.gregsadventure.card.Discard;
+import org.gregsquad.gregsadventure.game.Player;
+
+
 public class Game {
+    private ArrayList<Player> playerList;
+    private Stack donjonStack;
+    private Stack treasureStack;
+    private Discard donjonDiscard;
+    private Discard treasureDiscard;
     
 }
