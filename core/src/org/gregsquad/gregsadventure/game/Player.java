20eb53edcaa7d8f7a5b5ea72ca90c4794134e1ae
?FpAdxF&ci4Xpe <zentoxll@gmail.com>
2023-12-03 18:33:02 +0100

    Add player attributes and import statements

--- core/src/org/gregsquad/gregsadventure/game/Player.java
+++ core/src/org/gregsquad/gregsadventure/game/Player.java
@@ -1,5 +1,18 @@
 package org.gregsquad.gregsadventure.game;
 
+import org.gregsquad.gregsadventure.card.Card;
+import org.gregsquad.gregsadventure.card.Deck;
+
 public class Player {
+    private int level = 0;
+    private int damage = 0;
+    private int runAway = 0;
+
+    private card classe;
+    private card race;
+
+    private Deck deck;
+
     
+
 }
