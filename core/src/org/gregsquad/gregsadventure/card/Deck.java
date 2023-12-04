0b07fe96413c32dc663c8640323d50ba71f6b804
ZenT0x <zentoxll@gmail.com>
2023-12-04 08:28:39 +0100

    Refactor Deck class to remove inheritance and add new methods

--- core/src/org/gregsquad/gregsadventure/card/Deck.java
+++ core/src/org/gregsquad/gregsadventure/card/Deck.java
@@ -2,7 +2,10 @@ package org.gregsquad.gregsadventure.card;
 
 import java.util.ArrayList;
 
-public class Deck extends CardList{
+public class Deck{
+
+    protected int size;
+    protected ArrayList<Card> cards;
     
     Deck(){
         this.size = 0;
@@ -12,6 +15,18 @@ public class Deck extends CardList{
     Deck(ArrayList<Card> cards){
         this.size = cards.size();
         this.cards = cards;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public ArrayList<Card> getCards(){
+        return this.cards;
+    }
+
+    public Card getCard(int index){
+        return this.cards.get(index);
     }
 
     public void addCard(Card card){
