e9d24f8941a8af25c5307d324167fa1437b38965
Nico-Roelandt <nicolas.roelandt59@orange.fr>
2023-12-05 14:09:22 +0100

    début fight

--- core/src/org/gregsquad/gregsadventure/CoreLauncher.java
+++ core/src/org/gregsquad/gregsadventure/CoreLauncher.java
@@ -6,6 +6,7 @@ import org.gregsquad.gregsadventure.CardType.*;
 public class CoreLauncher {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
     }
 
     private ArrayList<Player> playerList;
@@ -27,7 +28,64 @@ public class CoreLauncher {
 
 
     public void fight(ArrayList<Player> playerList, Monster monster) {
-        // TODO implement here
+        int damagePlayer = playerList.get(0).getStrength();
+        damagePlayer = damagePlayer + playerList.get(1).getStrength();
+        int damageMonster = monster.getStrength();
+        //Show Interface Fight
+
+        //Show interface Card to all players
+        boolean cardHasBeenPlayed = false; // return of the interface
+
+        if(cardHasBeenPlayed){
+            fight(player, monster);
+            return;
+        } else {
+
+            //Show interface Action
+            String action = "Help"; // return of the interface
+
+            switch (action) {
+                case "Help":
+                    System.out.println("D2 : Help");
+                    //Show interface Select Player
+
+                    //Show proposal interface to players selected
+
+                    //Show proposal choice interface to player
+
+                    helpPlayer = new Player(); // return of the interface proposal choice
+                    
+                    if(helpPlayer == NULL){
+                        
+                    } else {
+                        allPlayerList = new ArrayList<Player>();
+                        allPlayerList.add(player);
+                        allPlayerList.add(helpPlayer);
+                        fight(player, monster);
+                        return;
+                    }
+                    break;
+                case "Run":
+                    System.out.println("D2 : Run");
+
+
+                    break;
+                case "Fight":
+                    System.out.println("D2 : Fight");
+
+                    break;
+
+
+                default:
+                    System.out.println("D2 : Error");
+                    break;
+            }
+
+
+        }
+
+
+
     }
 
     public void charity(){
