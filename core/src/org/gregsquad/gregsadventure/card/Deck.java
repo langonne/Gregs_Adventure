566e15ae96387eeaadedb8f72372ee07206f8097
?FpAdxF&ci4Xpe <zentoxll@gmail.com>
2023-12-31 15:00:11 +0100

    Add Serializable interface to Deck class

--- core/src/org/gregsquad/gregsadventure/card/Deck.java
+++ core/src/org/gregsquad/gregsadventure/card/Deck.java
@@ -1,6 +1,7 @@
 package org.gregsquad.gregsadventure.card;
 import java.util.ArrayList;
+import java.io.Serializable;
 
-public class Deck extends CardList{
+public class Deck extends CardList implements Serializable{
     protected CardList cards;
 }
