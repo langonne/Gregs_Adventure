bf5ce0becf52e1996f46c24e9be72e4ee57915ec
Nico-Roelandt <nicolas.roelandt59@orange.fr>
2024-01-09 16:04:14 +0100

    creation deck

--- core/src/org/gregsquad/gregsadventure/card/Deck.java
+++ core/src/org/gregsquad/gregsadventure/card/Deck.java
@@ -3,5 +3,6 @@ import java.util.ArrayList;
 import java.io.Serializable;
 
 public class Deck extends CardList implements Serializable{
-    protected CardList cards;
+
+    private int INITIAL_DECK_SIZE = 5;
 }
